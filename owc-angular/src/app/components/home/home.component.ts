3bc4d814b5ae94dc83e72fd3a7b298ac07178880
santiagorobinet <santiagorobinet@gmail.com>
2020-11-19 19:52:49 +0100

    configure encapsulation of home component

--- owc-angular/src/app/components/home/home.component.ts
+++ owc-angular/src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import 'owc-button';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import 'owc-custom-button/owcButton';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class HomeComponent implements OnInit {
 
