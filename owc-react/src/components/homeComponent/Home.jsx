9f62689b0caabfd9f2f46aaf5cab8c9060517352
santiagorobinet <santiagorobinet@gmail.com>
2020-11-19 17:27:23 +0100

    firts commit

---
+++ owc-react/src/components/homeComponent/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Home.css'
+import 'owc-button/owcButton.js'
+import 'owc-button/buttonAttributes.js'
+
+function HomeComponent (){
+
+    return(
+        <div className="button__container">
+            <owc-button></owc-button>
+        </div>
+    )
+}
+
+export default HomeComponent;
