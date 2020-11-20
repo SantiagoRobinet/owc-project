aad1df4df44e17b91e0b6725ea8dd7f62a4e5f8d
Gerard Ramon <gerardramonp@gmail.com>
2020-11-20 10:27:05 +0100

    fix style

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -31,12 +31,16 @@ export default {
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
 #app{
   width: 100%;
-  height: 800px;
+  height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
-
 }
 </style>
